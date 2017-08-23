ee517c49b6d630bd29aa62d3f1980f806e2912b2
Andrzej Ruszczewski <aruszczews001@cee.ema.ad.pwcinternal.com>
2017-08-23 11:03:38 +0200

    added count - analyze the behavior on subsequent requests with a prototype

--- src/main/java/com/mulesoft/training/MuleComponent.java
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -8,6 +8,12 @@ import org.mule.api.annotations.param.*;
 
 public class MuleComponent {
 
+	int count;
+
+	public MuleComponent() {
+		count = 1;
+	}
+
 	public Map<String, String> processMap(Map<String, String> input) {
 		input.put("processed by", "processMap");
 		return input;
@@ -34,11 +40,13 @@ public class MuleComponent {
 		return null;
 	}
 
-	public Map<String, String> processAll(@Payload Object input, @InboundHeaders("http.method") String method) {
-		Map<String, String> output = new HashMap<String, String>();
+	public Map<String, Object> processAll(@Payload Object input, @InboundHeaders("http.method") String method) {
+		Map<String, Object> output = new HashMap<String, Object>();
 		output.put("message", input.toString());
 		output.put("http.method", method);
 		output.put("processedBy", "processAll");
+		output.put("currentCount", count);
+		count++;
 		return output;
 	}
 
