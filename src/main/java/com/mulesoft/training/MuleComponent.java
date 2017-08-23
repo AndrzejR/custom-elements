2fa22cf8092a20f86c72750eddb243fc53e3f71c
Andrzej Ruszczewski <aruszczews001@cee.ema.ad.pwcinternal.com>
2017-08-23 10:30:42 +0200

    added map resolve by reflection

--- src/main/java/com/mulesoft/training/MuleComponent.java
+++ src/main/java/com/mulesoft/training/MuleComponent.java
@@ -4,24 +4,24 @@ import java.util.Map;
 
 public class MuleComponent {
 
-  public Map<String,String> processMap() {
-    // processMap implementation
-    return null;
-  }
+	public Map<String, String> processMap(Map<String, String> input) {
+		input.put("processed by", "processMap");
+		return input;
+	}
 
-  public Map<String,String> processArray() {
-    // processArray implementation
-    return null;
-  }
+	public Map<String, String> processArray() {
+		// processArray implementation
+		return null;
+	}
 
-  public Map<String,String> processString() {
-    // processString implementation
-    return null;
-  }
+	public Map<String, String> processString() {
+		// processString implementation
+		return null;
+	}
 
-  public Map<String,String> processString2() {
-    // processString2 implementation
-    return null;
-  }
+	public Map<String, String> processString2() {
+		// processString2 implementation
+		return null;
+	}
 
-} 
+}
